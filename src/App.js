596c3df1387effc8563ccbe8eb3f03f46325a02b
Sachin Pandey <skpandey885@gmail.com>
2023-03-09 22:37:45 +0530

    react-douter-dom setup - V4

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Base from './components/Base';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { } from 'reactstrap';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import About from './pages/About';
 
 function App() {
   return (
-    <>
-<Base>
- <h1>here we can add multiple components</h1>
+<BrowserRouter>
+<Routes>
+  <Route path = "/" element={<Home/>}/>
+  <Route path = "login" element={<Login/>}/>
+  <Route path = "signup" element={<Signup/>}/>
+  <Route path = "signup" element={<About/>}/>
 
-</Base>
-
-</>
+</Routes>
+</BrowserRouter>
   );
 }
 
